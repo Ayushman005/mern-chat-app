5526a2e5be3e6dc76062431fa4ddab09cade3f34
Ayushman Yadav <ayush@Ayushman.com>
2024-11-16 19:41:05 +0530

    2nd part done-ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+  return (
+    <div className='py-5 flex flex-col overflow-auto'>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
